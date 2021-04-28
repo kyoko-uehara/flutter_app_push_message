55a9b1f28ac7c2d5df450d67563c246f7b4b00b7
kyoko-uehara <kyoko.uehara@stv-tech.co.jp>
2021-04-28 22:01:14 +0900

    Firebase Cloud Messagingを使用してのPUSH通知

---
+++ android/app/src/main/kotlin/com/uehara/flutter_app_push_message/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.uehara.flutter_app_push_message
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+//参考URL（コピー元）
+//https://qiita.com/yana1316/items/9bd3806efc9cbdddcba9
+
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter FCM',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter FCM First Sample'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  String _token = "Waiting...";
+  String _message = "Waiting...";
+
+  @override
+  void initState() {
+    super.initState();
+
+    /// iOS用 PUSH通知の許可確認
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+
+    /// FCM token の取得
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _token = token;
+      });
+      print("token: $_token");
+    });
+
+    /// メッセージ受信時の処理
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        setState(() {
+          _message = "onMessage: $message";
+        });
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        setState(() {
+          _message = "onLaunch: $message";
+        });
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        setState(() {
+          _message = "onResume: $message";
+        });
+        print("onResume: $message");
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Your token'),
+            Text(_token),
+            Divider(),
+            Text('Message'),
+            Text(_message),
+          ],
+        ),
+      ),
+    );
+  }
+}
